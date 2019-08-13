c87e8919297a93ca46c19abc4b6e1f6c58a1b47a
tengyunzhi <tengyunzhi@sensetime.com>
2019-08-13 10:36:22 +0800

    add pnp

--- module/benchmark/test.cpp
+++ module/benchmark/test.cpp
@@ -50,7 +50,7 @@ Mat eulerAnglesToRotationMatrix(Vec3f &theta)
  
 }
 
-Eigen::Matrix<double,3,1> inv_trans(Mat& R, Mat& t,Eigen::Matrix<double,3,1>& p)
+Eigen::Matrix<double,3,1> inv_trans(const Mat& R, const Mat& t,const Eigen::Matrix<double,3,1>& p)
 {
     Eigen::Matrix<double,3,1> ps, t_e;
     Eigen::Matrix<double,3,3> Rinv_e;
@@ -63,7 +63,8 @@ Eigen::Matrix<double,3,1> inv_trans(Mat& R, Mat& t,Eigen::Matrix<double,3,1>& p)
 
 }
 
-Eigen::Matrix<double,3,1> trans(Mat& R, Mat& t,Eigen::Matrix<double,3,1>& p)
+
+Eigen::Matrix<double,3,1> trans(const Mat& R, const Mat& t,const Eigen::Matrix<double,3,1>& p)
 {
     Eigen::Matrix<double,3,1> t_e;
     Eigen::Matrix<double,3,3> R_e;
@@ -153,10 +154,11 @@ int main(){
     cout<<"transform c1 to c2:"<<endl<<"R1:"<<R1<<endl<<"t1:"<<t1<<endl;
     //undistortion projection
     Mat K = (Mat_<double>(3, 3) << 7.188560000000e+02, 0.000000000000e+00, 6.071928000000e+02, 0.000000000000e+00, 7.188560000000e+02, 1.852157000000e+02, 0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00);
+    cout<<"K:"<<endl<<K<<endl;
     Eigen::Matrix<double,3,3> K_e;
     cv2eigen(K,K_e);
 
-    Eigen::Matrix<double,3,1> p1_[Max], p2_[Max],p1uv[Max],p2uv[Max];//
+    Eigen::Matrix<double,3,1> p1_[Max], p2_[Max],p1uv[Max],p2uv[Max];//pi_ is normalized p1;uv is camera image
     Fi100{
         p1_[i] = p1[i];
         p1_[i](0,0) = p1_[i](0,0)/p1_[i](2,0);
@@ -184,7 +186,7 @@ int main(){
         point_.y = p2uv[i](1,0);
         points2.push_back(point_);
     }
-
+{//2 frame estimate E,R,t
     Mat E = findEssentialMat(points1,points2,K);
     cout<<"E_recover:"<<E<<endl;
     cout<<"E:"<< skew(t1)*R1 <<endl;
@@ -196,11 +198,82 @@ int main(){
     cout<<"p1:"<<p1[0]<<endl;
     cout<<"p2:"<<p2[0]<<endl;
     cout<<"p2-recovertrans:"<< trans(R_recover,t_recover,p1[0])<<endl;
-    cout<<"mask:"<<mask<<endl;
+    // cout<<"mask:"<<mask<<endl;
+}
+
+    vector<Point3d> point_3Dw;
+    Fi100{
+        Point3d point_;
+        point_.x = pw[i](0,0);///pw[i](2,0);
+        point_.y = pw[i](1,0);///pw[i](2,0);
+        point_.z = pw[i](2,0);
+        point_3Dw.push_back(point_);
+    }
+
+{//2 frame ba
 
 
-    vector<Point3d> point_3D;
+        // double camera_intrin_[4];
+        // camera_intrin_[0] = K.at<double>(0);
+        // camera_intrin_[1] = K.at<double>(2);
+        // camera_intrin_[2] = K.at<double>(4);
+        // camera_intrin_[3] = K.at<double>(5);
+
+        // Eigen::Matrix<double, 3, 2> camera_pose_;
+        // for (int i = 0; i < 3; i++)
+        // {
+        //     ((double *)(camera_pose_.data()))[i] = axis_angle_eigen(i, 0);
+        // }
+        // for (int i = 0; i < 3; i++)
+        // {
+        //     ((double *)(camera_pose_.data()))[i + 3] = t.at<double>(i);
+        // }
+        //     cout << "cam_pose_:" << camera_pose_ << endl;
+        // cout << " cam intrin:" << camera_intrin_[0] << "," << camera_intrin_[1] << "," << camera_intrin_[2] << "," << camera_intrin_[3] << "\n";
+        // ceres::Problem::Options problem_options;
+        // ceres::Problem problem(problem_options);
+        //         for (int i = 0; i < point_3D.size(); i++)
+        // {
+        //             problem.AddResidualBlock(new ceres::AutoDiffCostFunction<TReprojectionError, 2, 6, 3, 4>(
+        //                                  new TReprojectionError(points2[i].x, points2[i].y)),
+        //                              NULL,
+        //                              camera_pose_.data(),
+        //                              t3dpoint + i * 3,
+        //                              camera_intrin_);
+        //     problem.SetParameterBlockConstant(camera_intrin_);
+
+        //     vector<double*> parameter_block;
+        //     parameter_block.push_back(camera_pose_.data());
+        //     parameter_block.push_back(t3dpoint + i * 3);
+        //     parameter_block.push_back(camera_intrin_);
+        //     auto evaluatoption = ceres::Problem::EvaluateOptions();
+        //     evaluatoption.parameter_blocks = parameter_block;
+        //     cout<<"point"<<*(double*)(t3dpoint + i * 3)<<endl;
+        //     vector<double> residules;
+        //     double cost;
+        //     bool fa =  problem.Evaluate(ceres::Problem::EvaluateOptions(),&cost,&residules,NULL ,NULL);
+        //     cout<<"cost:"<<cost<<endl;
+        //     CHECK(fa);
+        //     cout<<"residuals:"<<residules[0]<<", "<<residules[1]<<endl;
+        //     problem.ceres::CostFunction::Evaluate();
+        // }
+}
+{//3d --> 2d
+
+    Mat pnp_recover_r_vec,pnp_recover_t_vec,distortion;
+
+    solvePnP(point_3Dw,points1,K,distortion,pnp_recover_r_vec,pnp_recover_t_vec,false,SOLVEPNP_ITERATIVE);
+    // solvePnPRansac(point_3Dw,points1,K,distortion,pnp_recover_r_vec,pnp_recover_t_vec,false,99999,(0.2F),0.99999999999,noArray(),SOLVEPNP_EPNP);
+    Mat pnp_recover_R;
+    Rodrigues(pnp_recover_r_vec,pnp_recover_R);
+    pnp_recover_R = pnp_recover_R * (-1);
+    pnp_recover_t_vec = pnp_recover_t_vec *(-1);
+    cout<<"pnprecover: R:"<<pnp_recover_R<<endl<<"t:"<<pnp_recover_t_vec<<endl;
+    cout<<"transform w to c1:"<<endl<<"R:"<<R.inv()<<endl<<"t:"<<(-1)*R.inv()*t<<endl;
+
+    cout<<"point1:"<<p1[1]<<endl<<trans(pnp_recover_R,pnp_recover_t_vec,pw[1])<<endl<<trans(R.inv(),(R.inv())*t*(-1),pw[1])<<endl;
+    cout<<"pointworld: "<<pw[1]<<endl<<inv_trans(pnp_recover_R,pnp_recover_t_vec,p1[1])<<endl<<trans(R,t,p1[1])<<endl;
     // triangulation(keypoints1, keypoints2, matches, R, t, point_3D);
-
+}
 
 }
