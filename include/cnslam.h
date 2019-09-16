99228f7cfa8c0bddec7307c98c0ce882b1a5bdb2
tengyunzhi <tengyunzhi@sensetime.com>
2019-09-16 18:07:47 +0800

    build lib

---
+++ include/cnslam.h
@@ -0,0 +1,6 @@
+#include "module/fematrix/fematrix.hpp"
+// #include "module/image_rectification/
+#include "module/pnp/pnp.hpp"
+#include "module/qr/qr.hpp"
+#include "module/stereomatch/stereomatch.hpp"
+#include "module/triangulation/triangulation.hpp"
